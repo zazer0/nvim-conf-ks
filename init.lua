f329e26172cc4ccb6a52598a730308a3296a472a
zazer1 <zac@strongcompute.com>
2025-02-19 00:40:21 -0800

    feat(syntax): add envrc as sh

--- init.lua
+++ init.lua
@@ -214,6 +214,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Have proper syntax highlighting for envs
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.env*',
+  callback = function()
+    vim.bo.filetype = 'sh'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
